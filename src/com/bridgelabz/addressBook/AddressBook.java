046027a79077abc5419cf219c0aab3443abf74a0
Uptech <uptech@trichemie.com>
2023-05-27 00:47:26 +0530

    Added:Java Program to add New Contact in Address Book

--- src/com/bridgelabz/addressBook/AddressBook.java
+++ src/com/bridgelabz/addressBook/AddressBook.java
@@ -1,4 +1,71 @@
 package com.bridgelabz.addressBook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+    ArrayList<Contact> contacts = new ArrayList();
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void displayContacts() {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Add Contact");
+            System.out.println("2. Display Contacts");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter first name: ");
+                    String firstName = scanner.nextLine();
+                    System.out.print("Enter last name: ");
+                    String lastName = scanner.nextLine();
+                    System.out.print("Enter address: ");
+                    String address = scanner.nextLine();
+                    System.out.print("Enter city: ");
+                    String city = scanner.nextLine();
+                    System.out.print("Enter state: ");
+                    String state = scanner.nextLine();
+                    System.out.print("Enter zip: ");
+                    String zip = scanner.nextLine();
+                    System.out.print("Enter phone number: ");
+                    String phoneNumber = scanner.nextLine();
+                    System.out.print("Enter email: ");
+                    String email = scanner.nextLine();
+
+                    Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                    addressBook.addContact(newContact);
+                    System.out.println("Contact added successfully!");
+                    break;
+                case 2:
+                    addressBook.displayContacts();
+                    break;
+                case 3:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
 }
+
+
